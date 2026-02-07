cf6e49ae77d07c225ffcac1d1a968f36552e4a59
itsseanez <74938477+itsseanez@users.noreply.github.com>
2026-02-06 22:17:14 -0500

    Add quantity to each ProductCard

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,19 @@
 // src/components/Layout.jsx
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import { Link } from "react-router";
 
 export default function Layout() {
   return (
     <>
-      <Header />           {/* Appears on every page */}
+      <header>
+      <Link to='/'><h1>My E-commerce Store</h1></Link>
+      <nav>
+        <Link to="/shop">Shop</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
+    </header>
       <main>
-        <Outlet />         {/* Renders the page-specific component */}
+        <Outlet />
       </main>
     </>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,38 @@
 import Button from "./Button";
 import styles from "../styles/ProductCard.module.css";
+import { useState } from "react";
 
-export default function ProductCard({ product, onClick, text, variant }) {
+export default function ProductCard({
+  product,
+  onClick,
+  text,
+  variant,
+  onQuantityChange,
+}) {
+  const [qty, setQty] = useState(1);
+
+  function increment() {
+    setQty((q) => q + 1);
+    onQuantityChange(product.id, qty + 1);
+  }
+
+  function decrement() {
+    if (qty > 1) {
+      setQty((q) => q - 1);
+      onQuantityChange(product.id, qty - 1);
+    }
+  }
+
   return (
     <div className={styles.productCard}>
       <img src={product.image} alt={product.title} />
       <h2>{product.title}</h2>
       <p>${product.price}</p>
+      <div className={styles.quantity}>
+        <Button onClick={decrement} text="-" variant="secondary"/>
+        <span>{qty}</span>
+        <Button onClick={increment} text="+" variant="secondary" />
+      </div>
       <Button text={text} variant={variant} onClick={() => onClick(product)} />
     </div>
   );
